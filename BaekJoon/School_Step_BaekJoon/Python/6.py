f423523f97c3dc35f8180778c8fc27a4767ba43d
test <sm46384638@naver.com>
2023-06-07 13:47:30 +0900

    ch6

--- BaekJoon/School_Step_BaekJoon/Python/6.py
+++ BaekJoon/School_Step_BaekJoon/Python/6.py
@@ -1,14 +1,13 @@
-a = int(input())
-data = []
-count =0
+N = int(input())
+cnt = N
 
-for i in range(a):
-    s = input()
-    data.append(s)
+for i in range(N):
+    word = input()
+    for j in range(0, len(word)-1):
+        if word[j] == word[j+1]:
+            pass
+        elif word[j] in word[j+1:]:
+            cnt -= 1
+            break
 
-for i in data :
-    for j in data :
-        if i == j :
-            count += 1
-
-print(a-count)
+print(cnt)
