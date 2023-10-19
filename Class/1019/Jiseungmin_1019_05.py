95db3c2860d8b1cda409473aceb77938b552f8b1
jiseungmin <sm46384638@naver.com>
2023-10-19 13:57:35 +0900

    Class 1019

--- Class/1019/Jiseungmin_1019_05.py
+++ Class/1019/Jiseungmin_1019_05.py
@@ -1,22 +1,33 @@
-# 제목:  DFS 알고리즘, 이것이 코딩 테스트다 with python
+# 제목:  그래프 BFS
 # 작성자 : 컴퓨터 공학부 지승민
 # 작성일 : 2023.10.19
 
-# DFS 함수 정의
-def dfs(graph, v, visited):
-# 현재 노드를 방문 처리
-  visited[v] = True
-  print(v, end=" ")
-# 현재 노드와 연결된 다른 노드를 재귀적으로 방문
-  for i in graph[v]:
-        if not visited[i]:
-              dfs(graph,i,visited)
-  
-graph = [[],[2,3,8],[1,7],[1,4,5],[3,5],[3,4],[7],[2,6,8], [1,7]]
-  
-visited = [False] * 9
-  
-dfs(graph, 1, visited)
+import queue
+
+def bfs(graph, start):
+      visited = {start}
+      que = queue.Queue()
+      que.put(start)
+      while not que.empty():
+            v = que.get()
+            print(v, end=' ')
+            nbr = graph[v] - visited
+            for u in nbr :
+                  visited.add(u)
+                  que.put(u)
+
+mygraph = {"A":{"B","C"},
+           "B":{"A","D"},
+           "C":{"A","D","E"},
+           "D":{"B","C","F"},
+           "E":{"C","G","H"},
+           "F":{"D"},
+           "G":{"E","H"},
+           "H":{"E","G"}}
+
+print("BFS :", end='')
+bfs(mygraph, "A")
+print()
 
 
 
--- Class/1019/Jiseungmin_1019_06.py
+++ Class/1019/Jiseungmin_1019_06.py
@@ -1,34 +1,30 @@
-# 제목:  그래프 BFS
+# 제목:  BFS 알고리즘 이것이 코딩 테스트다 WITH python
 # 작성자 : 컴퓨터 공학부 지승민
 # 작성일 : 2023.10.19
 
-import queue
+from collections import deque
 
-def bfs(graph, start):
-      visited = {start}
-      que = queue.Queue()
-      que.put(start)
-      while not que.empty():
-            v = que.get()
+#BFS 함수 정의
+def bfs(graph, start, visited):
+      # 큐 구현을 위해 deque 라이브러리 사용
+      queue = deque([start])
+      # 현재 노드를 방문 처리
+      visited[start] = True
+      # 큐가 빌 떄까지 반복
+      while queue:
+            #큐에서 하나의 원소를 뽑아 출력
+            v = queue.popleft()
             print(v, end=' ')
-            nbr = graph[v] - visited
-            for u in nbr :
-                  visited.add(u)
-                  que.put(u)
+            # 해당 원소와 연결된 아직 방문하지 않은 원소들을 큐에 삽입
+            for i in graph[v]:
+                  if not visited[i]:
+                        queue.append(i)
+                        visited[i] = True
 
-mygraph = {"A":{"B","C"},
-           "B":{"A","D"},
-           "C":{"A","D","E"},
-           "D":{"B","C","F"},
-           "E":{"C","G","H"},
-           "F":{"D"},
-           "G":{"E","H"},
-           "H":{"E","G"}}
+graph = [[],[2,3,8],[1,7],[1,4,5],[3,5],[3,4],[7],[2,6,8],[1,7]]
 
-print("BFS :", end='')
-bfs(mygraph, "A")
-print()
-
+visited = [False] * 9
+bfs(graph, 1, visited)
 
 
 
